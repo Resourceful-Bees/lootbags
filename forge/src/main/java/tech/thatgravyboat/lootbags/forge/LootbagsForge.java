c842b7c8cd3e9b9b596aca3401c9b6f8166fa9c6
ThatGravyBoat <thatgravyboat@gmail.com>
2022-10-09 11:23:16 -0230

    fixed forge 1.18

--- forge/src/main/java/tech/thatgravyboat/lootbags/forge/LootbagsForge.java
+++ forge/src/main/java/tech/thatgravyboat/lootbags/forge/LootbagsForge.java
@@ -1,10 +1,8 @@
 package tech.thatgravyboat.lootbags.forge;
 
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import tech.thatgravyboat.lootbags.Lootbags;
 import tech.thatgravyboat.lootbags.client.LootbagsClient;
@@ -12,10 +10,15 @@ import tech.thatgravyboat.lootbags.common.registry.forge.McRegistryImpl;
 
 @Mod(Lootbags.MOD_ID)
 public class LootbagsForge {
+
     public LootbagsForge() {
         Lootbags.init();
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         McRegistryImpl.REGISTRIES.values().forEach(deferredRegister -> deferredRegister.register(modEventBus));
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> LootbagsClient::init);
+        modEventBus.addListener(LootbagsForge::clientSetup);
+    }
+
+    public static void clientSetup(final FMLClientSetupEvent event) {
+        LootbagsClient.init();
     }
 }
