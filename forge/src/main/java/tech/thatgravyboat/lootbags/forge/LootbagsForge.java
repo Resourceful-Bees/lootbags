8735a0fc15406def1a6ea6f6d8d553c9712a816b
ThatGravyBoat <thatgravyboat@gmail.com>
2022-10-10 19:47:40 -0230

    fix client loading forge

--- forge/src/main/java/tech/thatgravyboat/lootbags/forge/LootbagsForge.java
+++ forge/src/main/java/tech/thatgravyboat/lootbags/forge/LootbagsForge.java
@@ -1,10 +1,8 @@
 package tech.thatgravyboat.lootbags.forge;
 
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import tech.thatgravyboat.lootbags.Lootbags;
 import tech.thatgravyboat.lootbags.client.LootbagsClient;
@@ -16,6 +14,10 @@ public class LootbagsForge {
         Lootbags.init();
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         McRegistryImpl.REGISTRIES.values().forEach(deferredRegister -> deferredRegister.register(modEventBus));
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> LootbagsClient::init);
+        modEventBus.addListener(LootbagsForge::clientSetup);
+    }
+
+    public static void clientSetup(FMLClientSetupEvent event) {
+        LootbagsClient.init();
     }
 }
